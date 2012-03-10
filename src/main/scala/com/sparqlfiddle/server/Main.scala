664d78539b43092a9177cce94bba9b95b7d9b832
Greg Heartsfield <scsibug@imap.cc>
2012-03-10 16:27:10 -0600

    Switch to Application/Component model.

--- src/main/scala/com/sparqlfiddle/server/Main.scala
+++ src/main/scala/com/sparqlfiddle/server/Main.scala
@@ -1,22 +1,45 @@
-import org.restlet.Server
+import org.restlet.{Restlet,Server,Application,Component}
+import org.restlet.routing.Router
 import org.restlet.data.Protocol
 import org.restlet.resource.{ServerResource,Get}
 
 
-class Main extends ServerResource {  
-
+class DefRes extends ServerResource {
   @Get
   override def toString():String = {  
-    return "hello, world";  
+    return "You've hit the default resource.";  
+  }
+}
+
+class SparqlProcessorResource extends ServerResource {
+  @Get
+  override def toString():String = {  
+    return "You've hit the SPARQL processor, using the GET method.";  
+  }
+}
+
+object MainApp extends Application {
+
+  override def createRoot():Restlet = {
+    println("MainApp starting");
+    val router = new Router(getContext())
+    router.attachDefault(classOf[DefRes])
+    router.attach("/sparqlDraft",classOf[SparqlProcessorResource])
+    router
   }
 
 }
 
 object Main {
-
   def main(args: Array[String]) {
-    println("Server starting");
-    new Server(Protocol.HTTP, 8080, classOf[Main]).start();
+    try {
+      val component = new Component()
+      component.getServers().add(Protocol.HTTP,8080)
+      component.getDefaultHost().attach(MainApp)
+      println("MainComponent starting");
+      component.start()
+    } catch {
+      case e:Exception => println("exception caught: "+e)
+    }
   }
-
 }
