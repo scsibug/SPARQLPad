2d0780a8925b98e3b05c99c1523771dcca97a779
Greg Heartsfield <scsibug@imap.cc>
2012-03-11 15:58:31 -0500

    JSON reply for sparqlDraft POSTs.

--- src/main/scala/com/sparqlfiddle/server/Main.scala
+++ src/main/scala/com/sparqlfiddle/server/Main.scala
@@ -1,16 +1,40 @@
+package com.sparqlfiddle.server
 import org.restlet.{Restlet,Server,Application,Component}
 import org.restlet.routing.Router
-import org.restlet.data.Protocol
-import org.restlet.resource.{ServerResource,Get}
-
+import org.restlet.data.{Protocol,Form,Status,MediaType}
+import org.restlet.resource.{ServerResource,Get,Post}
+import org.restlet.representation.{Representation,StringRepresentation}
+import org.restlet.ext.jackson.{JacksonRepresentation}
 import org.slf4j._
 
 class SparqlProcessorResource extends ServerResource {
   val logger = LoggerFactory.getLogger(classOf[SparqlProcessorResource])
   logger.info("Resource created")
+
   @Get
   override def toString():String = {  
     return "You've hit the SPARQL processor, using the GET method.";  
+  }
+
+  @Post  
+  def process(entity:Representation):Representation = {
+    var result:Representation = null
+    val form = new Form(entity)
+    // expect "sparql", "triples", and "triple-format" entries.
+    val sparqlraw = form.getFirstValue("sparql")
+    val triplesraw = form.getFirstValue("triples")
+    val tripleformatraw = form.getFirstValue("tripleformat")
+    logger.info("sparql was "+sparqlraw)
+    logger.info("triples was "+triplesraw)
+    logger.info("triples-format was "+tripleformatraw)
+    if (sparqlraw != null && triplesraw != null && tripleformatraw != null) {
+      setStatus(Status.SUCCESS_OK)
+      result = new JacksonRepresentation(DraftResponse("a sample response"))
+    } else {
+      setStatus(Status.CLIENT_ERROR_BAD_REQUEST)
+      result = new StringRepresentation("Missing form values", MediaType.TEXT_PLAIN)
+    }
+    result
   }
 }
 
