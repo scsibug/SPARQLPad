1eb4f1d2007f87a839cdea839a7c9f963112e42a
Greg Heartsfield <scsibug@imap.cc>
2012-03-11 23:04:24 -0500

    Can now serve both static and dynamic content (Twitter Bootstrap lib added).

--- src/main/scala/com/sparqlfiddle/server/Main.scala
+++ src/main/scala/com/sparqlfiddle/server/Main.scala
@@ -1,19 +1,28 @@
 package com.sparqlfiddle.server
 import org.restlet.{Restlet,Server,Application,Component}
 import org.restlet.routing.Router
+import org.restlet.resource.Directory
 import org.restlet.data.{Protocol,Form,Status,MediaType}
 import org.restlet.representation.{Representation,StringRepresentation}
 import org.restlet.ext.jackson.{JacksonRepresentation}
 import org.slf4j._
+import scala.collection.JavaConversions._
 
-object SparqlApp extends Application {
+object MainApp extends Application {
   val logger = LoggerFactory.getLogger("MainApp")
   override def createRoot():Restlet = {
-    println("MainApp starting");
+    // Support FILE for Directory restlet
+    getConnectorService().getClientProtocols().add(Protocol.FILE);
+    val directory = new Directory(getContext(), staticPath())
+    // wrap subordinate apps
     val router = new Router(getContext())
     router.attach("/sparqlDraft",classOf[SparqlProcessorResource])
+    router.attach("/index", directory)
+    router.attach("/static", directory)
     router
   }
+
+  def staticPath():String = new java.io.File("static_root").toURI().toString()
 }
 
 object Main {
@@ -23,7 +32,8 @@ object Main {
       System.setProperty("org.restlet.engine.loggerFacadeClass","org.restlet.ext.slf4j.Slf4jLoggerFacade")
       val component = new Component()
       component.getServers().add(Protocol.HTTP,8080)
-      component.getDefaultHost().attach(SparqlApp)
+      component.getClients().add(Protocol.FILE)
+      component.getDefaultHost().attach(MainApp)
       logger.info("MainComponent starting")
       component.start()
     } catch {
