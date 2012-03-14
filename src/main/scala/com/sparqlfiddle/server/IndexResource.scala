004584e8f43d0954fdcc159813703911b0a75710
Greg Heartsfield <scsibug@imap.cc>
2012-03-14 16:39:38 -0500

    Add static index resource.

---
+++ src/main/scala/com/sparqlfiddle/server/IndexResource.scala
@@ -0,0 +1,13 @@
+package com.sparqlfiddle.server
+
+import org.restlet.data.{Protocol,Form,Status,MediaType}
+import org.restlet.resource.{ServerResource,Get}
+import org.restlet.representation.{Representation,FileRepresentation}
+import org.slf4j._
+
+class IndexResource extends ServerResource {
+  @Get
+  def home(entity:Representation):Representation = {
+    new FileRepresentation(new java.io.File("static_root/index.html"), MediaType.TEXT_HTML)
+  }
+}
